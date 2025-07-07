3d2378b41c37434190adc1c0ca9ed61775fda065
Mohammed-Am <mh.amouzoun@gmail.com>
2025-07-07 23:18:56 +0100

    Edit Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 MatchTick. All rights reserved.</p>
+        <p>&copy; 2025 Afcon Worldcup Morocco. All rights reserved.</p>
       </div>
     </footer>
   );
