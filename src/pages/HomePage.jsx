7e565f7fc1687393fc5328c7c9f7b84d60f24a71
Mohammed-Am <mh.amouzoun@gmail.com>
2025-07-07 23:14:37 +0100

    add matches

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -59,7 +59,6 @@ const HomePage = () => {
   }, [loading, error, matches]);
 
   const upcomingMatches = matches.filter(match => match.status === 'upcoming');
-  const finishedMatches = matches.filter(match => match.status === 'finished');
 
   const last5Winners = [
     { year: 2023, winner: 'Ivory Coast' ,img:'https://r2.thesportsdb.com/images/media/team/badge/rwxuuu1455465643.png/tiny'},
@@ -99,30 +98,7 @@ const HomePage = () => {
         </div>
       )}
 
-      <h2 className="text-3xl font-bold text-center my-8 mt-12">Results</h2>
-      {finishedMatches.length === 0 ? (
-        <p className="text-center">No finished matches found.</p>
-      ) : (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {finishedMatches.map((match, index) => (
-            <div
-              key={match._id}
-              ref={el => matchCardsRef.current[index] = el}
-              className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300 dark:bg-gray-800 dark:text-gray-100"
-            >
-              <div className="p-6 flex items-center space-x-4">
-                {match.teamA.logoUrl && <img src={match.teamA.logoUrl} alt={match.teamA.name} className="w-12 h-12" />}
-                <div>
-                  <h3 className="text-xl font-bold">{match.teamA.name} {match.score?.teamA} - {match.score?.teamB} {match.teamB.name}</h3>
-                  <p className="text-gray-800">{new Date(match.date).toLocaleString()}</p>
-                  <p className="text-gray-700">{match.venue}</p>
-                </div>
-                {match.teamB.logoUrl && <img src={match.teamB.logoUrl} alt={match.teamB.name} className="w-12 h-12" />}
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
+      
 
       <h2 className="text-3xl font-bold text-center my-8 mt-12">Last 5 Winners</h2>
       <div className="flex flex-wrap justify-center gap-4">
